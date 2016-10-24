da63cee79d83c0442d7d61c6d2cf7f8f0e88c8db
Zach Garman <zachgarman@gmail.com>
2016-10-24 14:31:28 -0500

    done

--- public/scripts/rho.controller.js
+++ public/scripts/rho.controller.js
@@ -4,12 +4,9 @@ app.controller('MainController', MainController);
 function MainController (rho) {
   var self = this;
 
-  console.log('MainController loaded');
-
   self.showNames = function() {
     rho.getNames().then(function(names){
       self.students = names;
-      console.log(self.students);
     });
   };
 }
